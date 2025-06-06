6ad56085e0d715b1b67282bd481351e2f2849e5d
desainiravp <125334778+desainiravp@users.noreply.github.com>
2025-06-06 12:58:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Security Group
+resource "aws_security_group" "k8s_sg" {
+  name        = "k8s_sg"
+  description = "Allow Kubernetes ports"
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Kubernetes API server"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow NodePort services"
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Key Pair
+resource "aws_key_pair" "k8s_key" {
+  key_name   = "k8s_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# Master Node
+resource "aws_instance" "master" {
+  ami                    = "ami-0e2c8caa4b6378d8c" # Ubuntu 22.04
+  instance_type          = "t2.medium"
+  key_name               = aws_key_pair.k8s_key.key_name
+  vpc_security_group_ids = [aws_security_group.k8s_sg.id]
+
+  tags = {
+    Name = "k8s-master"
+  }
+}
+
+# Worker Nodes (2 nodes)
+resource "aws_instance" "worker" {
+  count                  = 2
+  ami                    = "ami-0e2c8caa4b6378d8c"
+  instance_type          = "t2.medium"
+  key_name               = aws_key_pair.k8s_key.key_name
+  vpc_security_group_ids = [aws_security_group.k8s_sg.id]
+
+  tags = {
+    Name = "k8s-worker-${count.index + 1}"
+  }
+}
